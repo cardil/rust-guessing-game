919ce6159adcb0e86dba2cc3660195cb98cafa91
Krzysztof Suszyński <krzysztof.suszynski@wavesoftware.pl>
2018-06-08 22:39:19 +0200

    Finished game

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,35 @@ fn main() {
   let secret = rand::thread_rng()
     .gen_range(1, 101);
 
-  println!("The secret number is: {}", secret);
+  let mut steps: u32 = 0;
+  loop {
+    println!("Please input your guess.");
 
-  println!("Please input your guess.");
+    let mut guess = String::new();
 
-  let mut guess = String::new();
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line");
 
-  io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+    let guess: u32 = match guess
+      .trim()
+      .parse() {
+        Ok(num) => num,
+        Err(_)  => {
+          println!("Please type a number!");
+          continue;
+        }
+      };
 
-  let guess: u32 = guess
-    .trim()
-    .parse()
-    .expect("Please type a number!");
+    steps = steps + 1;
 
-  println!("You guessed: {}", guess);
-
-  match guess.cmp(&secret) {
-    Ordering::Less    => println!("Too small!"),
-    Ordering::Greater => println!("Too big!"),
-    Ordering::Equal   => println!("You win!"),
+    match guess.cmp(&secret) {
+      Ordering::Less    => println!("Too small!"),
+      Ordering::Greater => println!("Too big!"),
+      Ordering::Equal   => {
+        println!("You win in {} guesses!", steps);
+        break;
+      }
+    }
   }
 }
